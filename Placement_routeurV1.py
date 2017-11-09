be727c1e5c948ff5b31b1bc699d9629a9db6d441
Guillaume POTIER <guillaume.potier@etu.univ-nantes.fr>
2017-11-09 16:38:18 +0100

    Update Placement_routeurV1.py

--- Placement_routeurV1.py
+++ Placement_routeurV1.py
@@ -4,25 +4,31 @@ def placement_routeurV1(plan,data):
 	budget = data[x]			#Addapter
 	hauteur_map = data[y]		#Addapter
 	largeur_map = data[z]		#Addapter
+	perim_routeur = data[w]		#Addapter
 	cout=0
-	while budget-coup>=0:  #tant que l'on a du budget on test
+	while budget-coup>=0:  																				#Tant que l'on a du budget on effectue le test
 		liste_score_routeur=[]
-		for i in range(hauteur_map):
-			coordX=i
+		for i in range(hauteur_map):  																	#On test chaque case de la matrice
+			coordX=i					
 			for k in range(largeur_map):
-				if map_travail[i][k]== '.' or map_travail[i][k] == 'w':					
-					coordY=K
-					liste_case_couverte = #fonctiondearthur(map_travail,coordX,coordY)
-					score=len(liste_case_couverte)*1000
+				coordY=k
+				if map_travail[i][k]== '.' or map_travail[i][k] == 'w':									#Si il est possible de mettre un routeur sur la case		
+					liste_case_couverte = #fonctiondearthur(map_travail,coordX,coordY,perim_routeur) 	#On regarde les case couvertes
+					score=len(liste_case_couverte)*1000													#On calacul le score
 					liste_score_routeur.append([score,coordX,coordY])
-		liste_score_routeur = sorted(mylist, key=lambda x: x[0])
-		liste_routeur.append([liste_score_routeur[0][1],liste_score_routeur[0][2]])
-		cout=cout+prix_routeur
-		#fonction de modification de map
-	return (liste_routeur)
+					
+					
+		liste_score_routeur = sorted(mylist, key=lambda x: x[0]).      									#Quand toute les case on été testé, on classe les routeur par leur score
+		liste_routeur.append([liste_score_routeur[0][1],liste_score_routeur[0][2]]). 					#On garde le meilleur et on le met en place
+		cout=cout+prix_routeur																			#On met à jour le budget pour vérifier quer l'on peut continuer
+		
+#Modification de la matrice pour que lesz cases déjà couverte ne puissent plus rapporter de points
+		
+		liste_case_couverte=#fonctiondearthur(map_travail,liste_score_routeur[0][1],liste_score_routeur[0][2],perim_routeur)
+		for i in range(len(liste_case_couverte)):
+			map_travail[liste_case_couverte[i][0]][liste_case_couverte[i][1]]]="w"
+			
+	return (liste_routeur).       																		#Dès que l'on a plus de budget, on renvoie la liste des routeurs
 			
 		
 				
-				
-				
-	
