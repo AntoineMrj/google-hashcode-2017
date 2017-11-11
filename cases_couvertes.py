73c4619609ce54e5578d36090a366e1b418573f5
Arthur_MILLET <arthur.millet@etu.univ-nantes.fr>
2017-11-11 13:15:00 +0100

    MAJ

--- cases_couvertes.py
+++ cases_couvertes.py
@@ -6,8 +6,10 @@
 # Created:    10/11/2017
 #-------------------------------------------------------------------------------
 
-from lecture_entete import *
+import lecture_entete
 
+#entrée : la matrice (avec les cases déjà couvertes remplacées (x)), coordonnées du routeur,
+#         rayon du routeur, largeur et hauteur de la matrice
 def cases_couvertes(matrice, coordX_routeur, coordY_routeur, radius, largeur_matrice, hauteur_matrice):
 	#matrice de travail
     map_travail = matrice
@@ -61,6 +63,7 @@ def cases_couvertes(matrice, coordX_routeur, coordY_routeur, radius, largeur_mat
                                 y_fin_case_test = y_coord
                             else:
                                 y_origin_case_test = y
+                                y_fin_case_test = y
                             #on compare la position de la ligne de la case étudié et la ligne du routeur
                             if (x_coord < x):
                                 x_origin_case_test = x_coord
@@ -70,7 +73,8 @@ def cases_couvertes(matrice, coordX_routeur, coordY_routeur, radius, largeur_mat
                                 x_fin_case_test = x_coord
                             else:
                                 x_origin_case_test = x
-                                
+                                x_fin_case_test = x
+
                             #on parcours l'ensemble du carré entre la case étudiée et le routeur pour vérifier la présence d'un mur ou non
                             #on parcours chaque colonne
                             for y_coord_case_test in range(y_origin_case_test, y_fin_case_test+1):
@@ -92,9 +96,11 @@ def cases_couvertes(matrice, coordX_routeur, coordY_routeur, radius, largeur_mat
                                     #si présence d'un mur on stop le parcours des colonnes restantes
                                     break
                             #on vérifie l'état de la variable de présence
-                            if (presence_mur == False):
-                            	#ajout de la case étudiée à la liste des cases couvertes
-                            	liste_cases_couvertes.append([x_coord, y_coord])
+                            if (presence_mur == False): #ajout de la case étudiée à la liste des cases couvertes
+                                matrice[y_coord][x_coord]='x'           #affichage cord
+                                liste_cases_couvertes.append([x_coord, y_coord])
+                                matrice[y][x]='r'           #affichage corrd routeur
+
 
     else:
         #si cellule incorrect (vie ou mur) on envoie 0 pour stoper la fonction
@@ -102,25 +108,41 @@ def cases_couvertes(matrice, coordX_routeur, coordY_routeur, radius, largeur_mat
 
     #fin
     return liste_cases_couvertes
-    
-    
+
+
+def affichage_matrice(mat):     #entrée : la matrice
+
+        for i in range (0, len(mat)):       #on parcours la totalité des lignes de la matrice
+            for j in range(0, len(mat[i])):     #on parcours la totalité du noimbre de colonnes de la matrice
+                print(mat[i][j], end="")        #on affiche l'élément sans retour à  la ligne
+            print()
+
+def creation_backbone():      #on affiche le backbone sur la map
+
+    coord_y_backbone = int(entete[6])    #on recupère les coord sur le return de la lecture entete
+    coord_x_backbone = int(entete[7])
+    matrice[coord_y_backbone][coord_x_backbone]='b'    #il prend la valeur de b
+
+
 if __name__ == '__main__':
-    #nom du fichier de la map
-    file_str = "map0.in"
-    
-    #on crée la matrice
-    matrice = creationMatrice(file_str)
-    
-	#bel affichage de la matrice
-	for i in range (0, len(matrice)):   #on parcours la totalité des lignes de la matrice
-	    for j in range(0, len(matrice[i])): #on parcours la totalité du noimbre de colonnes de la matrice
-	        print(matrice[i][j], end="")    #on affiche l'élément sans retour à la ligne
-        print() #on affiche un retour à la ligne
-    
-    print() #on affiche un retour à la ligne
-    
+
+#-------------------------------------------------
+    """ LECTURE DE LA MAP  : Code lecture_entete.py """
+    map="map0.in"               #Choix de la map   ( ex : charleston_road.in )
+    entete = lecture_entete.lectureEntete(map)
+    #print(entete)               #affichage de l'entète, on attrape ici toute les info du fichier .in (en char)
+    matrice = lecture_entete.creationMatrice(map)    #notre matrice
+#-------------------------------------------------
+
+    #creation_backbone()             #affiche sur la carte "b"
+
     #calcul des cases couvertes pour un routeur en 2, 2 et un rayon de 3
-    cases_couv = cases_couvertes(matrice, 2, 2, 3, 16, 7)
-    
+    cases_couv = cases_couvertes(matrice, 2, 2, 3, 22, 8)   #(matrice , coord_y_routeur1 , coord_x_routeur1 , int(entete[2]), int(entete[1]), int(entete[0]))
+
     #on affiche la liste des cases
     print(cases_couv)
+
+    #le nb de cases couv
+    print(len(cases_couv))
+
+    affichage_matrice(matrice)
