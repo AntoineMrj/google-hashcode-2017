1aa8ee19d6ddbe1afd58c03d38f331c0aacf8a6c
Bastien JEAN <bastien.jean@etu.univ-nantes.fr>
2017-11-11 16:25:19 +0100

    Replace lecture_entete.py, suppression de la partie création matrice (déplacée dans un fichier séparé)

--- lecture_entete.py
+++ lecture_entete.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 #-------------------------------------------------------------------------------
-# Purpose:
+# Purpose: renvoie l'entête complète du fichier d'entré dans une liste
 # Author:      Bastien
-# Created:    X/10/2017
+# Created:    11/11/2017
 #-------------------------------------------------------------------------------
 
-def lectureEntete(file_link):
-    fichier_map = open(file_link, "r")  #on ouvre le fichier à lire en mode lecture
+#entré : nom du fichier d'entré
+def lectureEntete(file_name):
+    fichier_map = open(file_name, "r")  #on ouvre le fichier d'entré en mode lecture
     premiere_ligne_entete = fichier_map.readline()  #on lit la première ligne
     deuxieme_ligne_entete = fichier_map.readline()  #on lit la deuxième ligne
     troisieme_ligne_entete = fichier_map.readline() #on lit la troisième ligne
@@ -30,31 +31,3 @@ def lectureEntete(file_link):
     fichier_map.close() #on ferme le fichier ouvert
 
     return nb_ligne, nb_col, portee_routeur, prix_fil, prix_routeur, budget_max, coord_y_backbone, coord_x_backbone #on renvoie l'ensemble des éléments
-
-
-def creationMatrice(file_link):
-    matrice = []    #matrice pincipale
-    ligneMatrice = []   #ligne d'une matrice
-
-    fichier_map = open(file_link, "r")  #on ouvre le fichier à  lire
-
-    for i in range(0, 3):   #on parcours l'entète
-        variable_inutile = fichier_map.readline()   #on stock temporairement les éléments trouvés
-        variable_inutile = []   #on vide les éléments trouvés précédemment
-
-    while 1:    #on parcours l'ensemble des caractères de la map
-        char = fichier_map.read(1)  #on récupère le caractère suivant
-        if char == '\n':    #si c'est une fin de ligne
-            matrice.append(ligneMatrice)    #on ajoute la ligne de matrice créée Ã  la matrice principale
-            ligneMatrice = []   #on reset la ligne de matrice pour passer  la suivante
-            pass
-        elif not char:  #si aucun caractère n'est récupéré, fin de fichier, on sort de la boucle
-            break
-        else:   #sinon (caractère trouvé)
-            ligneMatrice.append(char)   #on ajoute le caractère récupéré à  la fin de la ligne de matrice en cours de création
-
-    fichier_map.close() #on ferme le fichier ouvert
-
-    return matrice  #on renvoie la matrice créée
-
-""" Main -> Dans le code calcul_cases_couvertes de Arthur """
