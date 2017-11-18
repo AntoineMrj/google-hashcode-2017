6c07e8c01dda7159da2bd3d9b136e3e0ac6e906e
Romain Andrieux <romandrieux29@gmail.com>
2017-11-18 13:03:56 +0100

    Update outils.py

--- outils.py
+++ outils.py
@@ -9,6 +9,9 @@ import numpy as np
 import threading
 from multiprocessing import Process, Pipe
 
+def distance(pointA,pointB): #où pointA = (xA,yA) et pointB = (xB,yB)
+    dist = ((pointA[0]-pointB[0])**2 + (pointA[1]-pointB[1])**2)**0.5
+    return dist
 
 def relier2points(point1, point2):
     """
@@ -73,57 +76,94 @@ def relier2points(point1, point2):
     return points
 
 def relierRouteurs(listeRouteurs, backbone): 
-    
-    listeAretesC = [] #liste d'aretes du graphe complet de la forme (taille,sommet courant, sommet auquel on le relie,coordonnées des points de l'arete)
-    listeRouteurs = [backbone] + listeRouteurs
 
-    #===============================
-    # GENERATION DU GRAPHE COMPLET
-    #===============================
+    #===================================================
+    #DETERMINATION DES LISTES 'ARBRE', 'DIST' ET 'PRED'
+    #===================================================
     
-    for i in range(len(listeRouteurs)): #on éxecute une boucle n-1 fois avec n le nombre de routeurs
-        for j in range(len(listeRouteurs)): #on relie le sommet courant à tous les sommets sauf lui même
-            if listeRouteurs[i] != listeRouteurs[j]:
-                tuple = (len(relier2points(listeRouteurs[i],listeRouteurs[j])),listeRouteurs[i],listeRouteurs[j],relier2points(listeRouteurs[i],listeRouteurs[j]))
-                listeAretesC.append(tuple) #on ajoute l'arête à la liste
-                #print(listeRouteurs[i],'->',listeRouteurs[j]) -> à décommenter pour afficher les relations entres les sommets
+    n = len(liste_routeurs)
+    arbre = [backbone]
+    dist = [+float('inf')]*n
+    pred = [None]*n
     
-    #===============================
-    #      ALGORITHME DE PRIM
-    #===============================
-    succ = [] 
-    arbreMini = [] #arbre couvrant minimal contenant la liste des points où mettre de la fibre pour relier tous les routeurs
-    sommet = listeAretesC[0][1] #On prend arbitrairement comme premier sommet le sommet de départ (le backbone) de la première arrête de la liste des arêtes du graphe complet
-    listeSommetsRelies = [] #Liste contenant les sommets déjà reliés
-
-    while len(listeSommetsRelies)+1<len(listeRouteurs): #Tant que tous les routeurs ne sont pas reliés
-        #on ajoute à la liste des successeurs toutes les arrêtes ayant 'sommet' comme sommet de départ
-        for k in range(len(listeAretesC)):
-            if (listeAretesC[k][1] == sommet) and (listeAretesC[k][2] not in listeSommetsRelies) and (listeAretesC[k][1] not in listeSommetsRelies):
-                #print('ajout de : ',listeAretesC[k]) ->à décommenter pour afficher les arêtes qu'on ajoute à la liste des successeurs
-                succ.append(listeAretesC[k])
-        succ.sort() #déjà trié normalement a voir si je l'enlève
-        #print('Arrête choisie',succ[0]) -> à décommenter pour afficher l'arêtes qu'on décide d'ajouter à l'arbre couvrant minimal
-        arbreMini.append(succ[0]) #On ajoute a l'arbre couvrant minimal l'arrête de poids minimale des successeurs
-        listeSommetsRelies.append(succ[0][1]) #On ajoute le sommet courant à la liste des sommets reliés
-        sommet = succ[0][2] #Le prochain sommet correspondra au successeur du sommet précédent
-        succ = [] #On réinitialise la liste des successeurs
+    while len(arbre) < n+1:
         
-    #===============================================
-    # CONVERSION DE L'ARBRE EN UNE LISTE DE POINTS
-    #===============================================
-    
-    arbreFinal = []
-    
-    for l in range(len(arbreMini)):
-        for m in range(len(arbreMini[l][3])):
-            if arbreMini[l][3][m] not in arbreFinal: #On ne recopie pas les doublons
-                arbreFinal.append(arbreMini[l][3][m])
+        for i in range(n):
+            
+                dist_i = distance(arbre[-1],liste_routeurs[i])
+                
+                if dist[i] != None:
+                    if dist_i < dist[i]:
+                        dist[i] = dist_i
+                        pred[i] = arbre[-1]
+        
+        mini = +float('inf')
+        for d in dist:
+          if d != None:
+            if d < mini:
+                mini = d
+        
 
-    arbreFinal.pop(0) #On enlève le premier éléement de la liste, c'est à dire le backbone
-    print(arbreFinal)
+        for j in range(len(dist)):
+            if dist[j] == mini:
+                arbre.append(liste_routeurs[j])
+                dist[j] = None
+                break
+
     
-    return arbreFinal
+    #============================================================================
+    #RELIER LES ROUTEURS A LEUR PREDECESSEUR AVEC LA FIBRE EN PARTANT DU BACKBONE
+    #============================================================================
+
+    fibre = []
+    nb_cable = len(fibre)
+    
+    for i in range(0,len(liste_routeurs)):
+        portion_chemin_i = relier2points(pred[i],liste_routeurs[i])
+        fibre += portion_chemin_i
+        nb_cable += len(portion_chemin_i)
+        
+    fibre_ordonee = [backbone]
+    fibre_finale = []
+    fin = False
+    
+    while fin == False:
+        
+        for i in range(len(fibre)):
+            if ((((fibre[i][0]+1,fibre[i][1]) in fibre_ordonee) or ((fibre[i][0]+1,fibre[i][1]) in liste_routeurs)) \
+            or (((fibre[i][0]-1,fibre[i][1]) in fibre_ordonee) or ((fibre[i][0]-1,fibre[i][1]) in liste_routeurs)) \
+            or (((fibre[i][0],fibre[i][1]+1) in fibre_ordonee) or ((fibre[i][0],fibre[i][1]+1) in liste_routeurs)) \
+            or (((fibre[i][0],fibre[i][1]-1) in fibre_ordonee) or ((fibre[i][0],fibre[i][1]-1) in liste_routeurs)) \
+            or (((fibre[i][0]+1,fibre[i][1]+1) in fibre_ordonee) or ((fibre[i][0]+1,fibre[i][1]+1) in liste_routeurs)) \
+            or (((fibre[i][0]-1,fibre[i][1]-1) in fibre_ordonee) or ((fibre[i][0]-1,fibre[i][1]-1) in liste_routeurs)) \
+            or (((fibre[i][0]+1,fibre[i][1]-1) in fibre_ordonee) or ((fibre[i][0]+1,fibre[i][1]-1) in liste_routeurs)) \
+            or (((fibre[i][0]-1,fibre[i][1]+1) in fibre_ordonee) or ((fibre[i][0]-1,fibre[i][1]+1) in liste_routeurs)) \
+            and (fibre[i] not in fibre_ordonee and fibre[i] not in liste_routeurs)):
+                fibre_ordonee.append(fibre[i])
+        
+        test = True
+        for elmt in fibre:
+            if elmt not in fibre_ordonee:
+                test = False
+            else:
+                test = test and True
+        
+        if test == True:
+            fin = True
+        else:
+            fin = False
+            
+        print(test)
+    
+    for couple in fibre_ordonee:
+        if couple not in fibre_finale:
+            fibre_finale.append(couple)
+
+    for couple in fibre_finale:
+        if couple == backbone:
+            fibre_finale.remove(couple)
+    
+    return fibre_finale
 
 
 #entête : nombre de cables posés, liste des coordonnées des cases de cable, nombre de routeur, liste des coordonnées des routeurs
